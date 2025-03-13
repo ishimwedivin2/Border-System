a2eb6efebd5a1bfb8a851dc78d2888dcf22cc45f
Ishimwe Divin <ishimwedivin2@gmail.com>
2025-03-13 23:14:10 +0200

    commit

--- src/main/java/com/border/border/controller/UserController.java
+++ src/main/java/com/border/border/controller/UserController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/us")
 public class UserController {
     @Autowired
     private UserService userService;
